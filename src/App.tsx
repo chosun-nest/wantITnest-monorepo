879d8612a4716664212fd6556cb82680e7b73962
Angol Minsu <steinsgate888@gmail.com>
2025-03-08 20:26:59 +0900

    base one

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import {
+  RouterProvider,
+  Navigate,
+  createBrowserRouter,
+} from "react-router-dom";
 import { useState } from "react";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import PasswdReset from "./routes/passwd-reset";
+import Layout from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile/",
+        element: <Profile />,
+      },
+    ],
+  },
+  // 이 윗 줄 까진 Layout의 children임.
+  {
+    path: "/login",
+    element: <NavigateToHome />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+  {
+    path: "/password-reset",
+    element: <PasswdReset />,
+  },
+]);
 
 function App() {
-  return <>Nest-FE is Start now!</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+      Nest-FE is Start now!
+    </>
+  );
+}
+
+function NavigateToHome() {
+  try {
+    return <Login />;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default App;
