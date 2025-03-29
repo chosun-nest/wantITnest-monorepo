4fc66f099ffca4eea18f65fead6ece474d07d5fb
Virtus_Chae <ybd4731@gmail.com>
2025-03-29 11:01:00 +0900

    feat: url 변경 -> member -> members

--- src/main/java/com/virtukch/nest/member/controller/MemberController.java
+++ src/main/java/com/virtukch/nest/member/controller/MemberController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/member")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
 
