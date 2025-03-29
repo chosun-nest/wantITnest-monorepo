502478d7f84ae04a1190b5649809cb92f5b9e4c9
Virtus_Chae <ybd4731@gmail.com>
2025-03-29 11:48:11 +0900

    feat: Auto Increment 설정 추가

--- src/main/java/com/virtukch/nest/activity/model/Activity.java
+++ src/main/java/com/virtukch/nest/activity/model/Activity.java
@@ -1,6 +1,8 @@
 package com.virtukch.nest.activity.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Activity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long activityId;
     private Long memberId;
     private String activityDate;
--- src/main/java/com/virtukch/nest/career/model/Career.java
+++ src/main/java/com/virtukch/nest/career/model/Career.java
@@ -1,6 +1,8 @@
 package com.virtukch.nest.career.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Career {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long careerId;
     private Long memberId;
     private String careerStartDate;
