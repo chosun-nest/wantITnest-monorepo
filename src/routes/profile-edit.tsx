b4c2eae089b226f29867ba9524a0f3c40042c6a4
azureun <euny1387@gmail.com>
2025-04-02 10:34:45 +0900

    chore : png 경로 수정

--- src/routes/profile-edit.tsx
+++ src/routes/profile-edit.tsx
@@ -1,5 +1,13 @@
 export default function ProfileEdit() {
-    return <div>
-        profile-edit.tsx
-    </div>;
+    return (
+        <div className="flex pt-6 px-10 pt-25 text-[20px] font-bold text-[#00256c] font-sans"
+            style={{
+                fontFamily: "Monomaniac One",   //Tailwind는 기본적으로 이 폰트를 모르기 때문에 설정함.
+            }}
+        >
+        프로필 수정 및 개인정보 변경
+        </div>
+
+    );
 }
+  
