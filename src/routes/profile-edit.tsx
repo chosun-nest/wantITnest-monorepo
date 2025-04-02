ad973d9b70f7ac6ccec5d31b61523a6525f1a8b1
azureun <euny1387@gmail.com>
2025-04-02 13:19:31 +0900

    feat : 프로필 수정 페이지 - 내 프로필 컴포넌트 만들고 로컬에서 수정 가능

--- src/routes/profile-edit.tsx
+++ src/routes/profile-edit.tsx
@@ -1,13 +1,37 @@
-export default function ProfileEdit() {
-    return (
-        <div className="flex pt-6 px-10 pt-25 text-[20px] font-bold text-[#00256c] font-sans"
-            style={{
-                fontFamily: "Monomaniac One",   //Tailwind는 기본적으로 이 폰트를 모르기 때문에 설정함.
-            }}
-        >
-        프로필 수정 및 개인정보 변경
-        </div>
+import Navbar from "../components/layout/navbar";
+import MyProfileComponent from "../components/profile/profile-edit-myprofile-component";
+import { useRef, useEffect, useState } from "react";
 
+export default function ProfileEdit() {
+    const navbarRef = useRef<HTMLDivElement>(null);
+    const [navHeight, setNavHeight] = useState(0);
+
+    useEffect(() => {
+        if (navbarRef.current) {
+            setNavHeight(navbarRef.current.offsetHeight);
+        }
+    }, []);
+
+    return (
+        <>
+        <div ref={navbarRef}>
+            <Navbar />
+        </div>
+        <div className="flex px-8 bg-white min-h-screen" style={{ marginTop: navHeight }}>
+            <div
+                //className="flex pt-40 px-20 pt-25 text-[20px] font-bold text-[#00256c] font-sans"
+                className="pt-16 px-4 text-[20px] font-bold text-[#00256c] font-sans"
+                style={{
+                    fontFamily: "Monomaniac One",
+                }}
+            > 계정 정보
+            </div>
+
+            <div className="w-full px-4">
+                <MyProfileComponent />
+            </div>
+            
+        </div>
+        </>
     );
 }
-  
