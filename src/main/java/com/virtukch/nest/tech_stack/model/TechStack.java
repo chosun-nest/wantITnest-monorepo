21979fe6753941c1944ccde5ac296348cb786df3
Virtus_Chae <ybd4731@gmail.com>
2025-04-04 01:08:12 +0900

    feat: 기술 스택 데이터 로더 추가

--- src/main/java/com/virtukch/nest/tech_stack/model/TechStack.java
+++ src/main/java/com/virtukch/nest/tech_stack/model/TechStack.java
@@ -4,14 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tech_stack")
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TechStack {
