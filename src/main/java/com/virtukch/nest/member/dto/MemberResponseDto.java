c9f1884957a89be2746874d78dd300d2e00a3662
Virtus_Chae <ybd4731@gmail.com>
2025-04-03 23:46:22 +0900

    feat: memberService 가 memberId 를 기준으로 DB 조회하게 변경

--- src/main/java/com/virtukch/nest/member/dto/MemberResponseDto.java
+++ src/main/java/com/virtukch/nest/member/dto/MemberResponseDto.java
@@ -1,12 +1,18 @@
 package com.virtukch.nest.member.dto;
 
+import com.virtukch.nest.member.model.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@Builder
+@NoArgsConstructor
 public class MemberResponseDto {
 
     private Long memberId;
     private String memberEmail;
+    private Role memberRole;
 }
