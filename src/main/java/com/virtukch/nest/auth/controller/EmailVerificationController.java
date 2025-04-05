dcf648672800246058f7e5316f09ada3119410a1
Virtus_Chae <ybd4731@gmail.com>
2025-04-05 16:18:48 +0900

    feat: Email Verfication Get -> Body + Dto

--- src/main/java/com/virtukch/nest/auth/controller/EmailVerificationController.java
+++ src/main/java/com/virtukch/nest/auth/controller/EmailVerificationController.java
@@ -1,5 +1,7 @@
 package com.virtukch.nest.auth.controller;
 
+import com.virtukch.nest.auth.dto.EmailVerificationCodeRequestDto;
+import com.virtukch.nest.auth.dto.EmailVerificationRequestDto;
 import com.virtukch.nest.auth.service.EmailVerificationService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -15,15 +17,15 @@ public class EmailVerificationController {
 
     @Operation(summary = "이메일 인증 코드 전송", description = "입력된 이메일 주소로 6자리 인증 코드를 전송합니다.")
     @PostMapping("/send-code")
-    public ResponseEntity<String> sendCode(@RequestBody String email) {
-        emailVerificationService.sendVerificationCode(email);
+    public ResponseEntity<String> sendCode(@RequestBody EmailVerificationCodeRequestDto emailVerificationCodeRequestDto) {
+        emailVerificationService.sendVerificationCode(emailVerificationCodeRequestDto.getEmail());
         return ResponseEntity.ok("Verification code sent");
     }
 
     @Operation(summary = "인증 코드 검증", description = "이메일과 인증 코드를 검증합니다.")
     @PostMapping("/verify-code")
-    public ResponseEntity<String> verifyCode(@RequestBody String email, @RequestBody String code) {
-        emailVerificationService.verifyCode(email, code);
+    public ResponseEntity<String> verifyCode(@RequestBody EmailVerificationRequestDto emailVerificationRequestDto) {
+        emailVerificationService.verifyCode(emailVerificationRequestDto.getEmail(), emailVerificationRequestDto.getCode());
         return ResponseEntity.ok("Email verified successfully");
     }
 }
