8187d75d0007708527f4b828867287b27d3d0a90
Angol Minsu <steinsgate888@gmail.com>
2025-04-14 00:13:10 +0900

    fix: rebuild{index.tsx}전면 수정

--- src/api/common/common.tsx
+++ src/api/common/common.tsx
@@ -1,17 +1,17 @@
 // 팀장님꺼니 건들지 마라
-import API from "../index";
+import { API } from "../index";
 
 export const getTech = async () => {
-  const res = API.get("/api/v1/tech-stacks");
+  const res = API.get("/api/v1/tech-stacks", { headers: { skipAuth: true } });
   return (await res).data;
 };
 
 export const getInterests = async () => {
-  const res = API.get("/api/v1/interests");
+  const res = API.get("/api/v1/interests", { headers: { skipAuth: true } });
   return (await res).data;
 };
 
 export const getDepartments = async () => {
-  const res = API.get("/api/v1/departments");
+  const res = API.get("/api/v1/departments", { headers: { skipAuth: true } });
   return (await res).data;
 };
