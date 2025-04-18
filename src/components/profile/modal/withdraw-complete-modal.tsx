16ea00798fe7315baec70455e97b5e7b1b5c9920
azureun <euny1387@gmail.com>
2025-04-18 19:19:58 +0900

    build: 회원 탈퇴 비밀번호 확인 추가함
    추후 BE에서 비밀번호 확인 post 요청 만들어지면 api 수정할 예정

--- src/components/profile/modal/withdraw-complete-modal.tsx
+++ src/components/profile/modal/withdraw-complete-modal.tsx
@@ -1,12 +1,18 @@
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function WithdrawCompleteModal({ onClose }: { onClose: () => void }) {
   const navigate = useNavigate();
 
   const handleConfirm = () => {
+    localStorage.removeItem("accesstoken"); // 토큰 제거
     onClose();            // 모달 닫기
     navigate("/");        // 홈페이지(루트)로 이동
   };
+  useEffect(() => {
+    // 혹시 잊고 안 넣었을 경우를 대비해서 제거
+    localStorage.removeItem("accesstoken");
+  }, []);
 
   return (
     <div className="fixed inset-0 bg-gray-400 bg-opacity-50 z-50 flex items-center justify-center">
@@ -15,7 +21,7 @@ export default function WithdrawCompleteModal({ onClose }: { onClose: () => void
         <p className="text-sm text-gray-600 mb-6">이용해주셔서 감사합니다.</p>
         <button
           onClick={handleConfirm}
-          className="px-4 py-2 bg-blue-500 text-white rounded"
+          className="px-4 py-2 bg-blue-900 text-white rounded"
         >
           확인
         </button>
