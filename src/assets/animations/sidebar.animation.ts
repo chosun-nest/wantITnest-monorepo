4212d977ab4bfaeee547279d7d65bf73cdf542c7
Angol Minsu <steinsgate888@gmail.com>
2025-04-28 18:22:55 +0900

    build: sidebar 추가 및 비컴포넌트 파일명 tsx->ts로 변경

---
+++ src/assets/animations/sidebar.animation.ts
@@ -0,0 +1,20 @@
+// src/components/sidebar/Sidebar.animation.ts
+import { keyframes } from "styled-components";
+
+export const slideIn = keyframes`
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+export const slideOut = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`;
