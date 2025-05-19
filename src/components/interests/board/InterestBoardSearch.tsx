70bb864e5a3888e3a68a89d2bb68cd0084023779
azureun <euny1387@gmail.com>
2025-05-20 01:04:43 +0900

    style: 검색바 크기 수정

--- src/components/interests/board/InterestBoardSearch.tsx
+++ src/components/interests/board/InterestBoardSearch.tsx
@@ -1,4 +1,5 @@
 // 게시글 검색
+// 게시글 검색
 interface InterestBoardSearchProps {
   searchKeyword: string;
   setSearchKeyword: (value: string) => void;
@@ -9,17 +10,20 @@ export default function InterestBoardSearch({
   setSearchKeyword,
 }: InterestBoardSearchProps) {
   return (
-    <div className="flex w-full gap-2 mb-4">
-      <input
-        type="text"
-        placeholder="게시글 제목을 검색해보세요"
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
-        className="flex-grow p-3 border rounded"
-      />
-      <button className="px-7 py-2 text-white bg-[#002F6C] rounded">
-        검색
-      </button>
+    <div className="flex justify-center mb-4">
+      <div className="flex w-full max-w-xl gap-2">
+        <input
+          type="text"
+          placeholder="게시글 제목을 검색해보세요"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+          className="flex-grow px-3 py-2 border rounded"
+        />
+        <button className="px-5 py-2 text-white bg-[#002F6C] rounded">
+          검색
+        </button>
+      </div>
     </div>
   );
 }
+
