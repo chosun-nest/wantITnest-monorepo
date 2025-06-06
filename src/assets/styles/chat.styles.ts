862f9065586e7edbbc2af809cff91ca3eac48760
Angol Minsu <steinsgate888@gmail.com>
2025-06-07 01:06:45 +0900

    build : 채팅방 구현중에 ㅣㅆ음

--- src/assets/styles/chat.styles.ts
+++ src/assets/styles/chat.styles.ts
@@ -1,8 +1,112 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-interface ContainerProps {
-  navbarHeight: number;
-}
-export const Container = styled.div<ContainerProps>`
+export const Container = styled.div<{ navbarHeight: number }>`
   padding-top: ${({ navbarHeight }) => `${navbarHeight}px`};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  padding-inline: 4px;
+  height: calc(100vh - ${({ navbarHeight }) => navbarHeight}px);
+`;
+
+export const ChatContainer = styled.div<{ isMobile: boolean }>`
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid #ccc;
+  border-radius: 8px;
+  border: 1px solid #002f6c;
+  overflow: hidden;
+  width: 100%;
+  height: 100%; /* Container가 차지하는 남은 높이를 모두 사용 */
+
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          flex-direction: column;
+          max-width: 100%;
+          padding: 0 15px;
+        `
+      : css`
+          flex-direction: row;
+          border-radius: 8px;
+          width: 90%;
+          max-width: 1200px;
+          height: 80vh;
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+          padding: 0;
+        `}
+`;
+
+export const MessagesContainer = styled.div<{ isMobile?: boolean }>`
+  flex: 1;
+  overflow-y: auto;
+  background-color: #fff;
+
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          // ChatContainer에 패딩을 줬으므로 여기서는 0으로 유지해도 됩니다.
+          padding: 10px 0; /* 위아래 10px, 좌우 0 */
+        `
+      : css`
+          flex: 3;
+          padding: 20px;
+          border-right: 1px solid #eee;
+        `}
+`;
+
+export const InputContainer = styled.div<{ isMobile?: boolean }>`
+  display: flex;
+  background-color: #f1f1f1;
+
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          // ChatContainer에 패딩을 줬으므로 여기서는 0으로 유지해도 됩니다.
+          padding: 10px 0; /* 위아래 10px, 좌우 0 */
+          border-top: 1px solid #ccc;
+        `
+      : css`
+          flex: 1;
+          flex-direction: column;
+          padding: 20px;
+          justify-content: flex-end;
+          border-top: none;
+        `}
+`;
+
+export const Underbar = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-radius: 8px;
+  border: 1px solid #002f6c;
+  width: 100%;
+  min-height: 40px;
+  height: 15%;
+  max-height: 100px;
+  box-shadow: 0 -1px 2px rgba(0, 0, 0, 0.1);
+  margin-top: 10px;
+  margin-bottom: 10px;
+  background-size: 200% 100%;
+  background-position: right center;
+`;
+
+export const UnderbarItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: "Monomaniac One", sans-serif;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  &:hover {
+    background-color: #002f6c38;
+  }
+  svg {
+    width: 24px;
+    height: 24px;
+    color: #002f6c;
+  }
 `;
