313f2d1250751214cc0f7f2765489103c536090d
Virtus_Chae <ybd4731@gmail.com>
2025-06-07 10:57:06 +0900

    custom exception 추가

---
+++ src/main/java/com/virtukch/nest/history/exception/EmptyHistoryCreationException.java
@@ -0,0 +1,8 @@
+package com.virtukch.nest.history.exception;
+
+public class EmptyHistoryCreationException extends RuntimeException {
+
+    public EmptyHistoryCreationException(String message) {
+        super(message);
+    }
+}
