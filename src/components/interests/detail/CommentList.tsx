4058e815856f210cd0b5ff0565366c61b85f91eb
azureun <euny1387@gmail.com>
2025-06-09 23:23:56 +0900

    feat: 댓글 기능 구현 완료 및 InterestAPI author:id,name으로 수정

--- src/components/interests/detail/CommentList.tsx
+++ src/components/interests/detail/CommentList.tsx
@@ -1,25 +1,24 @@
-// 댓글 배열을 parentId 기반으로 계층 구조로 재정렬하고, 각 댓글을 CommentItem으로 렌더링
 import CommentItem from "./CommentItem";
-import type { Comment, BoardType } from "../../../types/api/comments";
+import type { CommentWithReplies, BoardType } from "../../../types/api/comments";
 
 interface CommentListProps {
-  comments: Comment[];
+  comments: CommentWithReplies[];
   boardType: BoardType;
   postId: number;
   onRefresh: () => void;
 }
 
 export default function CommentList({ comments, boardType, postId, onRefresh }: CommentListProps) {
-  // 현재 로그인한 사용자 ID를 localStorage에서 직접 확인
-  const memberId = Number(localStorage.getItem("memberId"));
-  const isLoggedIn = !Number.isNaN(memberId);
-
   const buildCommentTree = () => {
-    const map = new Map<number, Comment & { replies: Comment[] }>();
-    const roots: (Comment & { replies: Comment[] })[] = [];
+    const map = new Map<number, CommentWithReplies>();
+    const roots: CommentWithReplies[] = [];
 
     comments.forEach((comment) => {
-      map.set(comment.commentId, { ...comment, replies: [] });
+      const commentWithReplies: CommentWithReplies = {
+        ...comment,
+        replies: comment.replies ?? [],
+      };
+      map.set(comment.commentId, commentWithReplies);
     });
 
     map.forEach((comment) => {
@@ -30,7 +29,6 @@ export default function CommentList({ comments, boardType, postId, onRefresh }: 
         if (parent) {
           parent.replies.push(comment);
         } else {
-          // 부모 댓글을 못 찾으면 루트로 취급 (예외 처리)
           roots.push(comment);
         }
       }
@@ -49,7 +47,6 @@ export default function CommentList({ comments, boardType, postId, onRefresh }: 
           comment={comment}
           boardType={boardType}
           postId={postId}
-          memberId={isLoggedIn ? memberId : undefined}
           onRefresh={onRefresh}
         />
       ))}
