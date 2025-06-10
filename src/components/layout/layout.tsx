bd9dc81401a57f592e9df63ce86028fa74bfd164
Angol Minsu <steinsgate888@gmail.com>
2025-06-10 13:52:21 +0900

    fix: 탈모 완치

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,7 +4,6 @@ import Ai from "./ai";
 import { useEffect, useRef, useState } from "react";
 import { NavbarHeightContext } from "../../context/NavbarHeightContext";
 import Footer from "./footer";
-import "./layout.css";  // ✅ 추가
 
 export default function Layout() {
   const navbarRef = useRef<HTMLDivElement>(null);
@@ -18,14 +17,10 @@ export default function Layout() {
 
   return (
     <NavbarHeightContext.Provider value={{ navbarHeight, setNavbarHeight }}>
-      <div className="layout-container">
       <Navbar ref={navbarRef} />
       <Ai />
-      <div className="content-container" style={{ paddingTop: navbarHeight }}>
-        <Outlet />
-      </div>
+      <Outlet />
       <Footer />
-      </div>
     </NavbarHeightContext.Provider>
   );
 }
