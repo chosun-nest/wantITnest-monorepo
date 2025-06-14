d04e75731aea9360fc4f7ad64be01082124bf8c0
Virtus_Chae <ybd4731@gmail.com>
2025-06-14 23:59:21 +0900

    code reformat + custom jpa method 에 @Transactional 추가

--- src/main/java/com/virtukch/nest/chatting_room/service/ChattingRoomService.java
+++ src/main/java/com/virtukch/nest/chatting_room/service/ChattingRoomService.java
@@ -4,10 +4,10 @@ import com.virtukch.nest.chatting_room.dto.ChattingRoomRequestDto;
 import com.virtukch.nest.chatting_room.dto.ChattingRoomResponseDto;
 import com.virtukch.nest.chatting_room.model.ChattingRoom;
 import com.virtukch.nest.chatting_room.repository.ChattingRoomRepository;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,30 +15,33 @@ public class ChattingRoomService {
 
     private final ChattingRoomRepository chattingRoomRepository;
 
-    public ChattingRoomResponseDto createNewChattingRoomWithName(ChattingRoomRequestDto requestDto) {
+    public ChattingRoomResponseDto createNewChattingRoomWithName(
+        ChattingRoomRequestDto requestDto) {
         LocalDateTime currentDateTime = LocalDateTime.now();
         ChattingRoom chattingRoomToSave = ChattingRoom.builder()
-                .chattingRoomName(requestDto.getChattingRoomName())
-                .createdAt(currentDateTime)
-                .lastChattedAt(currentDateTime)
-                .build();
+            .chattingRoomName(requestDto.getChattingRoomName())
+            .createdAt(currentDateTime)
+            .lastChattedAt(currentDateTime)
+            .build();
         ChattingRoom savedChattingRoom = chattingRoomRepository.save(chattingRoomToSave);
         return toDto(savedChattingRoom);
     }
 
-    public ChattingRoomResponseDto updateChattingRoomNameById(Long id, ChattingRoomRequestDto requestDto) {
+    public ChattingRoomResponseDto updateChattingRoomNameById(Long id,
+        ChattingRoomRequestDto requestDto) {
         ChattingRoom chattingRoomToUpdate = chattingRoomRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("채팅방이 존재하지 않습니다."));
+            .orElseThrow(() -> new IllegalArgumentException("채팅방이 존재하지 않습니다."));
         chattingRoomToUpdate = ChattingRoom.builder()
-                .chattingRoomId(chattingRoomToUpdate.getChattingRoomId())
-                .chattingRoomName(requestDto.getChattingRoomName())
-                .createdAt(chattingRoomToUpdate.getCreatedAt())
-                .lastChattedAt(chattingRoomToUpdate.getLastChattedAt())
-                .build();
+            .chattingRoomId(chattingRoomToUpdate.getChattingRoomId())
+            .chattingRoomName(requestDto.getChattingRoomName())
+            .createdAt(chattingRoomToUpdate.getCreatedAt())
+            .lastChattedAt(chattingRoomToUpdate.getLastChattedAt())
+            .build();
         ChattingRoom updatedChattingRoom = chattingRoomRepository.save(chattingRoomToUpdate);
         return toDto(updatedChattingRoom);
     }
 
+    @Transactional
     public void deleteChattingRoomById(Long id) {
         if (!chattingRoomRepository.existsById(id)) {
             throw new IllegalArgumentException("채팅방이 존재하지 않습니다.");
@@ -48,10 +51,10 @@ public class ChattingRoomService {
 
     private ChattingRoomResponseDto toDto(ChattingRoom room) {
         return ChattingRoomResponseDto.builder()
-                .chattingRoomId(room.getChattingRoomId())
-                .chattingRoomName(room.getChattingRoomName())
-                .createdAt(room.getCreatedAt())
-                .lastChattedAt(room.getLastChattedAt())
-                .build();
+            .chattingRoomId(room.getChattingRoomId())
+            .chattingRoomName(room.getChattingRoomName())
+            .createdAt(room.getCreatedAt())
+            .lastChattedAt(room.getLastChattedAt())
+            .build();
     }
 }
