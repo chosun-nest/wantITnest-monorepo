8e5e410a87d4cedcae43df8b6474c6cc0e73e6fe
dlwlgur02 <dlwlgur02@gmail.com>
2025-06-22 20:50:05 +0900

    Feature/ji-hyuk : 브랜치 최신화

--- src/main/java/com/virtukch/nest/chatting_room/model/ChattingRoom.java
+++ src/main/java/com/virtukch/nest/chatting_room/model/ChattingRoom.java
@@ -1,5 +1,6 @@
 package com.virtukch.nest.chatting_room.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class ChattingRoom {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chattingRoomId;
 
+    @Column(nullable = false, unique = true)
     private String chattingRoomName;
 
     private LocalDateTime createdAt;
