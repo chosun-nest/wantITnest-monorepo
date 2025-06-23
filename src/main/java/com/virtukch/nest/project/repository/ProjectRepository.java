583c9291062fa4833881e54f1a822ec5fd8cc257
dlwlgur02 <dlwlgur02@gmail.com>
2025-06-23 18:29:41 +0900

    Feature/ji-hyuk : 프로젝트 검색 문제 해결

--- src/main/java/com/virtukch/nest/project/repository/ProjectRepository.java
+++ src/main/java/com/virtukch/nest/project/repository/ProjectRepository.java
@@ -16,7 +16,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     Page<Project> findAll(Pageable pageable);
 
     // 특정 태그에 해당하는 게시물 조회 (페이징 적용)
-    @Query("SELECT p FROM Project p JOIN ProjectTag pt ON p.memberId = pt.projectId WHERE pt.tagId IN :tagIds")
+    @Query("""
+    SELECT p FROM Project p 
+    JOIN ProjectTag pt ON p.projectId = pt.projectId 
+    WHERE pt.tagId IN :tagIds
+    """)
     Page<Project> findByTagIds(@Param("tagIds") List<Long> tagIds, Pageable pageable);
 
     // 제목 검색
