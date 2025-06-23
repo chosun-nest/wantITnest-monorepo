ffa4f2dcede110e09cf09a035fa2ec91b1f4ccc4
haileymmm <hrmin7dev@gmail.com>
2025-06-23 22:34:41 +0900

    api/ai/ai 변경

--- frontend/src/api/ai/ai.ts
+++ frontend/src/api/ai/ai.ts
@@ -1,9 +1,9 @@
 export async function fetchChatBotAnswer(question: string): Promise<string> {
-  //const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   // 그냥 잠깐 동작하는지 테스트
-  // const res = await fetch(`${API_BASE_URL}/api/ai/chatbot`, {
-  const res = await fetch("http://localhost:8001/api/ai/chatbot", {
+  const res = await fetch(`${API_BASE_URL}/api/ai/chatbot`, {
+  // const res = await fetch("http://localhost:8001/api/ai/chatbot", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ question }),
