3f3471e4a8867b6c0d55d0a3ad4514801cb498eb
AtraFelis <cotjs0321@naver.com>
2025-07-18 14:42:42 +0900

    chore: 필요 없는 주석문 삭제 및 줄바꿈 제거

--- backend/src/main/java/com/virtukch/nest/auth/service/AuthService.java
+++ backend/src/main/java/com/virtukch/nest/auth/service/AuthService.java
@@ -65,11 +65,7 @@ public class AuthService {
         // 4. memberDepartment 데이터 삽입 (다대다 테이블 고려)
         memberDepartmentService.create(memberId, signupRequestDto.getDepartmentIdList());
 
-        // memberInterest 데이터는 받지 읺기로 함.
-//        // 5. memberInterest 데이터 삽입 (다대다 테이블 고려)
-//        memberInterestService.create(memberId, signupRequestDto.getInterestIdList());
-
-        // 6. memberTechStack 데이터 삽입 (다대다 테이블 고려)
+        // 5. memberTechStack 데이터 삽입 (다대다 테이블 고려)
         memberTechStackService.create(memberId, signupRequestDto.getTechStackIdList());
 
         return SignupResponseDto.builder()
